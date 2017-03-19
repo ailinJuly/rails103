553881036bf8ad6383b727f536c357786039f06b
jy <ailinjulygao@gmail.com>
2017-03-19 15:07:18 +0800

    join or quit groupaciton

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -50,7 +50,31 @@ end
     redirect_to groups_path, alarm: "Group deleted"
  end
 
+ def join
+     @group = Group.find(params[:id])
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论版成功！"
+      else
+        flash[:warning] = "你已经是本讨论区成员了！"
+      end
+        redirect_to group_path(@group)
+ end
+
+
+ def quit
+   @group = Group.find(params[:id])
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已退出本讨论版！"
+      else
+        flash[:warning] = "你不是本讨论区成员，不能退出！"
+      end
+        redirect_to group_path(@group)
+ end
+
  private
+
 
 def find_group_and_check_permisstion
   @group = Group.find(params[:id])
@@ -62,13 +86,4 @@ end
  def group_params
    params.require(:group).permit(:title, :description)
  end
-
-
-
-
-
-
-
-
-
 end
